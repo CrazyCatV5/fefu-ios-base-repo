3b3416b72944e9931eb17b221d73b0afe257019a
zahar <zahark13@gmail.com>
2021-11-21 02:39:40 +1000

    полилиния не работает

--- fefuactivity/Дз3/MyMapView.swift
+++ fefuactivity/Дз3/MyMapView.swift
@@ -22,9 +22,20 @@ class MyMapViewController: UIViewController{
             if oldValue == nil, let myLocation = myLocation{
                 let region = MKCoordinateRegion(center: myLocation.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
                 myMap.setRegion(region, animated: true)
+                myLocationHistory.append(myLocation)
             }
         }
     }
+    
+    fileprivate var myLocationHistory: [CLLocation] = []{
+        didSet{
+            let coordinates = myLocationHistory.map{$0.coordinate}
+            let route = MKPolyline(coordinates: coordinates, count: coordinates.count)
+            route.title = "здесь могла быть ваша реклама"
+            myMap.addOverlay(route)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         myLocationManager.delegate = self
@@ -44,5 +55,19 @@ extension MyMapViewController: CLLocationManagerDelegate{
             return
         }
         myLocation = curentLocation
+    }
+}
+
+extension MyMapViewController: MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer{
+        if let polyline = overlay as? MKPolyline{
+            let render = MKPolylineRenderer(polyline: polyline)
+            render.fillColor = UIColor.blue
+            render.strokeColor = UIColor.blue
+            render.lineWidth = 5
+            return render
+            
+        }
+        return MKOverlayRenderer(overlay: overlay)
     }
 }
