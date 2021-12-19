95b7ec44a57f2fee321af50afa027f2e8e6ddf96
zahar <zahark13@gmail.com>
2021-12-19 22:35:02 +1000

    start

---
+++ fefuactivity/Дз4/EnterViewController.swift
@@ -0,0 +1,33 @@
+//
+//  EnterViewController.swift
+//  fefuactivity
+//
+//  Created by wsr5 on 19.12.2021.
+//
+
+import UIKit
+
+class EnterViewController: UIViewController {
+    @IBAction func Button(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var LoginText: UITextField!
+    @IBOutlet weak var PasswordText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ fefuactivity/Дз4/RegisterViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RegisterViewController.swift
+//  fefuactivity
+//
+//  Created by wsr5 on 19.12.2021.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var LoginText: UITextField!
+    @IBOutlet weak var PasswordText: UITextField!
+    @IBOutlet weak var SecondPasswordText: UITextField!
+    @IBOutlet weak var Name: UITextField!
+    @IBOutlet weak var Gender: UITextField!
+    @IBAction func Button(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
